c38b44343eb8dc23f901212ea196e5e62e41d443
TirumalaNaik12 <tirukush2000@gmail.com>
2025-07-25 15:42:32 +0530

    Backend 4rd commit

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,28 @@
+﻿
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path = require('path');
 const connectDB = require('./config/db');
 
+
 dotenv.config();
-const app = express();
 
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Connect DB
 connectDB();
 
-// Routes
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/blogs', require('./routes/blogRoutes'));
 app.use('/api/comments', require('./routes/commentRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
